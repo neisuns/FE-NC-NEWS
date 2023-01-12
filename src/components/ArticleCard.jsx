5eb1673fd14816b2ea4da1736d7fdec79fb13876
Neha Sundar <nehasundar@Nehas-Air.broadband>
2023-01-12 11:21:15 +0000

    added body to single article as well as capitalise topic title

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -3,6 +3,7 @@ import {Link} from "react-router-dom"
 const ArticleCard = ({ article }) => {
     const {author, created_at, title, topic, article_id } = article;
     const myDate = created_at.substring(0, 10);
+    const myTopic = topic.charAt(0).toUpperCase() + topic.slice(1);
 
     return (
         <section className="ArticleCard">
@@ -10,6 +11,7 @@ const ArticleCard = ({ article }) => {
                 <h3>{title}</h3>
             </Link>
             <div className="Article">
+                <p>Topic: {myTopic}</p>
                 <p>Author: <span>{author}</span></p>
                 <p>Date: {myDate}</p>
             </div>
--- src/components/SingleArticle.jsx
+++ src/components/SingleArticle.jsx
@@ -4,7 +4,7 @@ import { fetchArticleID } from "../API";
 
 const SingleArticle = () => {
     const { article_id } = useParams();
-    const [article, setArticle] = useState();
+    const [article, setArticle] = useState({});
     const [isLoading, setIsLoading] = useState(true);
     useEffect(() => {
         setIsLoading(true);
@@ -13,18 +13,20 @@ const SingleArticle = () => {
             setIsLoading(false);
         });
     }, [article_id])
+    
+    const {author, created_at, title, topic, body } = article;
 
     if (isLoading) {
         return <p>Loading... Please wait</p>;
     } 
     return (
-        <div className='Article'>
+        <article className='Article'>
             <h2>{article.title}</h2>
             <p>Author: <span>{article.author}</span></p>
             <p>Date: {article.created_at}</p>
-            <p>Topic: {article.topic}</p>
-            
-        </div>
+            <p>Topic: {article.topic.charAt(0).toUpperCase() + topic.slice(1)}</p>
+            <main>{article.body}</main>
+        </article>
     )
 }
 
